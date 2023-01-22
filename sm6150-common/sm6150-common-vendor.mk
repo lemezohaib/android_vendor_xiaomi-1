aa05d3af64e99e6cd6f532630f4a6bf8a99f2900
basamaryan <basam.aryan@gmail.com>
2023-01-22 09:42:09 +0000

    sm6150-common: Decommonize prebuilt display stack
    Change-Id: I9cc7fe555de468068eec87aebabdc0c77d51cc07

--- sm6150-common/sm6150-common-vendor.mk
+++ sm6150-common/sm6150-common-vendor.mk
@@ -233,14 +233,12 @@ PRODUCT_PACKAGES += \
     libconfigdb \
     libcpion \
     libdataitems \
-    libdisplayqos \
     libdpmqmihal \
     libdrmfs \
     libdrmtime \
     libdsi_netctrl \
     libgdtap \
     libhdcpsrm \
-    libhdr_tm \
     libhexagon_nn_stub \
     libhta_hexagon_runtime \
     libizat_client_api \
@@ -282,7 +280,6 @@ PRODUCT_PACKAGES += \
     libqdp \
     libqisl \
     libqrtr \
-    libqseed3 \
     libqsocket \
     libqtikeymaster4 \
     librcc \
@@ -290,18 +287,11 @@ PRODUCT_PACKAGES += \
     libril-qc-logger \
     librilqmiservices \
     librpmb \
-    libsdedrm \
-    libsdm-color \
-    libsdm-colormgr-algo \
-    libsdm-diag \
-    libsdm-disp-vndapis \
-    libsdmextension \
     libsdsprpc \
     libsoc_helper \
     libssd \
     libsystem_health_mon \
     libtime_genoff \
-    libtinyxml2_1 \
     libtrustedapploader \
     libwms \
     libwqe \
